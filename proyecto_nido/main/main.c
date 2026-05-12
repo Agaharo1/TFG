48fb3c9fd50af46cc1c1806eb17806c352ce8106
Agaharo1 <115159667+Agaharo1@users.noreply.github.com>
2026-05-12 17:53:24 +0200

    Updates

--- proyecto_nido/main/main.c
+++ proyecto_nido/main/main.c
@@ -92,6 +92,12 @@ static void event_handler(void *arg, esp_event_base_t event_base, int32_t event_
 // --- 3. INICIALIZACIÓN DE WI-FI ---
 void wifi_init_sta(void)
 {
+    s_wifi_event_group = xEventGroupCreate();
+    if (s_wifi_event_group == NULL) {
+        ESP_LOGE(TAG, "No se pudo crear el grupo de eventos Wi-Fi");
+        return;
+    }
+
     esp_netif_init();
     esp_event_loop_create_default();
     esp_netif_create_default_wifi_sta();
@@ -240,9 +246,9 @@ void app_main(void)
             ESP_LOGE(TAG, "Fallo al inicializar la cámara: 0x%x", err);
         }
 
-        // Dormir 1 hora exacta (3600 segundos)
-        tiempo_dormir_us = 3600ULL * 1000000ULL;
-        ESP_LOGI(TAG, "Entrando en Deep Sleep durante 1 hora...");
+        // Dormir 30 minutos exactos (1800 segundos)
+        tiempo_dormir_us = 1800ULL * 1000000ULL;
+        ESP_LOGI(TAG, "Entrando en Deep Sleep durante 30 minutos...");
     }
     else
     {
